7062774c2a1cacd74aa00c91077fcd3bf05bdd04
Keaton-Brewster <keaton.brewster@gmail.com>
2021-04-28 06:44:46 -0500

    initial commit

---
+++ client/src/utils/works.js
@@ -0,0 +1,45 @@
+const routinify = {
+    project_name: 'Routinify',
+    project_redirect_app: 'https://routinify.herokuapp.com/',
+    project_redirect_github: 'https://github.com/Keaton-Brewster/project-2',
+    project_description: `A simple task management / communication application for 
+    anyone looking to manage things within a group. Create groups, tasks, and routines and 
+    add any other users to your groups to participate in group task management`
+}
+
+const noteTaker = {
+    project_name: 'Notes Web App',
+    project_redirect_app: 'https://notetaker-keatonbrewster.herokuapp.com/',
+    project_redirect_github: 'https://github.com/Keaton-Brewster/Note-Taking-WebApp',
+    project_description: `A clean and simple web application for creating, saving, and editing notes
+    for personal, or professional use`
+}
+
+const pair = {
+    project_name: 'PAIR',
+    project_redirect_app: 'https://brandyquinlan.github.io/PAIR/',
+    project_redirect_github: 'https://github.com/brandyquinlan/PAIR',
+    project_description: `
+    A group collaboration, PAIR is designed to give you inspiration in the kitchen,
+    and behind the bar by providing you with recipes for food AND drinks, all in one place`
+}
+
+const weatherDashboard = {
+    project_name: 'Weather Dashboard',
+    project_redirect_app: 'https://keaton-brewster.github.io/Weather-Dashboard/',
+    project_redirect_github: 'https://github.com/Keaton-Brewster/Weather-Dashboard',
+    project_description: `A simple and concise app for checking the weather and saving your frequent
+    locations. Great for day-to-day use, or for planning a trip to a new location!`
+}
+
+const workDayPlanner = {
+    project_name: 'Work Day Planner',
+    project_redirect_app: 'https://keaton-brewster.github.io/Work-Day-Planner/',
+    project_redirect_github: 'https://github.com/Keaton-Brewster/Work-Day-Planner',
+    project_description: `A tool for planning out your work day, and tracking tasks hourly. Updates automatically
+    to display current hour, and hours past`
+}
+
+const works = [routinify, noteTaker, pair, weatherDashboard, workDayPlanner]
+export default works
+ 
