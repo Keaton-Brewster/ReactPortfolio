fdda2c42291c5bea9f681039be5ff443d70e6349
Keaton-Brewster <keaton.brewster@gmail.com>
2021-04-28 20:43:51 -0500

    broke heroku on last push. hopefully fixing now

--- server/index.js
+++ server/index.js
@@ -11,6 +11,10 @@ app.use(express.urlencoded({
 app.use(express.json());
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
 require('../controllers/api')(app);
 
 app.listen(PORT, () => {
