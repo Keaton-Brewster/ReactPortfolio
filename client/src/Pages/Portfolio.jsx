83e61acaa024ac023308394de52d7831f3b06590
Keaton-Brewster <keaton.brewster@gmail.com>
2021-05-05 15:44:43 -0500

    portfolio styling

--- client/src/Pages/Portfolio.jsx
+++ client/src/Pages/Portfolio.jsx
@@ -1,39 +1,40 @@
-import { useContext } from 'react';
-import { Context } from '../utils/GlobalState';
-import { PanelGroup, Panel, Container, Sidebar, Content } from 'rsuite';
-import WorkTemplate from '../components/WorkTemplate'
-import Works from '../utils/works';
+import { useContext } from "react";
+import { Context } from "../utils/GlobalState";
+import { PanelGroup, Panel, Container, Sidebar, Content } from "rsuite";
+import WorkTemplate from "../components/WorkTemplate";
+import Works from "../utils/works";
 
 function Portfolio() {
-    const [store, dispatch] = useContext(Context);
+  const [store, dispatch] = useContext(Context);
 
-    return (
-
-        <Container>
-            <Sidebar>
-                <Panel className="mt-1">
-                    <img id="workImg" src={store.workImg} alt="Screeenshot of displayed portfolio work" />
-                </Panel>
-            </Sidebar>
-            <Content>
-                <PanelGroup
-                    accordion
-                    defaultActiveKey={0}>
-                    {Works.map((work, i) => {
-                        return (
-                            <Panel
-                                onClick={() => dispatch({ type: "change work image", payload: work.img })}
-                                header={<h3>{work.name}</h3>}
-                                eventKey={i}
-                                key={i}>
-                                <WorkTemplate work={work} />
-                            </Panel>
-                        )
-                    })}
-                </PanelGroup>
-            </Content>
-        </Container>
-    )
+  return (
+    <Container>
+      <Content>
+        <PanelGroup accordion defaultActiveKey={0}>
+          {Works.map((work, i) => {
+            return (
+              <Panel
+                onClick={() =>
+                  dispatch({ type: "change work image", payload: work.img })
+                }
+                header={<h3>{work.name}</h3>}
+                eventKey={i}
+                key={i}
+              >
+                <img
+                  id="workImg"
+                  src={store.workImg}
+                  alt="Screeenshot of displayed portfolio work"
+                  className="float-right"
+                />
+                <WorkTemplate work={work} />
+              </Panel>
+            );
+          })}
+        </PanelGroup>
+      </Content>
+    </Container>
+  );
 }
 
 export default Portfolio;
