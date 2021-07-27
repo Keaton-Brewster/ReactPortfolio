7529865908456cc48eb59ab2d7e15298b7332547
Keaton-Brewster <keaton.brewster@gmail.com>
2021-07-27 09:28:12 -0500

    update resume

--- client/src/utils/Scene.js
+++ client/src/utils/Scene.js
@@ -1,10 +1,10 @@
 import * as THREE from "three";
-// import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-  90,
+  75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
@@ -18,43 +18,57 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 // Off the bat, the camera is going to be placed at the middle of the scene,
 // To get a better perspective on our scene, we need to move the camera back a little bit
 
-// Torus Know shape
-// const geomotry = new THREE.TorusKnotGeometry(20, 8, 120, 32);
-// const material = new THREE.MeshStandardMaterial({
-//   color: 0xffffff,
-// });
-// const torusKnot = new THREE.Mesh(geomotry, material);
-// torusKnot.position.set(-25, 0, 60);
-// // Add torus knot
-// scene.add(torusKnot);
+// Torus Knot shape
+const geomotry = new THREE.TorusKnotGeometry(20, 8, 120, 32);
+const material = new THREE.MeshStandardMaterial({
+  color: 0xffffff,
+});
+const torusKnot = new THREE.Mesh(geomotry, material);
+torusKnot.position.set(-150, -50, -120);
+scene.add(torusKnot);
 
 // point lights
-// const pointLight1 = new THREE.PointLight(0xffffff);
-// pointLight1.position.set(15, 15, 15);
-// const pointLight2 = new THREE.PointLight(0xffffff);
-// pointLight2.position.set(-50, -45, -30);
+const pointLight1 = new THREE.PointLight(0xffffff);
+pointLight1.position.set(15, 15, 15);
+const pointLight2 = new THREE.PointLight(0xffffff);
+pointLight2.position.set(-50, -45, -30);
+const pointLight3 = new THREE.PointLight(0xffffff);
+pointLight3.position.set(55, -50, -160);
+const lightHelper = new THREE.PointLightHelper(pointLight2);
+scene.add(pointLight1, pointLight3, lightHelper);
+const controls = new OrbitControls(camera, renderer.domElement);
 
-// const lightHelper = new THREE.PointLightHelper(pointLight2);
+function randomCircle() {
+  const geomotry = new THREE.SphereGeometry(0.25, 24, 24);
+  //   const geomotry = new THREE.TubeGeometry(THREE.Curve(THREE.Vector2));
+  const material = new THREE.MeshStandardMaterial(0xffffff);
+  const shape = new THREE.Mesh(geomotry, material);
 
-// scene.add(pointLight1, pointLight2, lightHelper);
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(100));
 
-// const controls = new OrbitControls(camera, renderer.domElement);
+  shape.position.set(x, y, z);
+  scene.add(shape);
+}
+Array(200).fill().forEach(randomCircle);
 
-// function addShape() {
-//   const geomotry = new THREE.SphereGeometry(0.25, 24, 24);
-//   //   const geomotry = new THREE.TubeGeometry(THREE.Curve(THREE.Vector2));
-//   const material = new THREE.MeshStandardMaterial(0xffffff);
-//   const shape = new THREE.Mesh(geomotry, material);
+function randomTorusKnot() {
+  const [w, x, y, z] = Array(4)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(5, 80));
+  const geomotry = new THREE.TorusKnotGeometry(w, x, y, z);
+  const material = new THREE.MeshBasicMaterial(0xffffff);
+  const knot = new THREE.Mesh(geomotry, material);
 
-//   const [x, y, z] = Array(3)
-//     .fill()
-//     .map(() => THREE.MathUtils.randFloatSpread(100));
+  const [a, b, c] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(24));
+  knot.position.set(a, b, c);
 
-//   shape.position.set(x, y, z);
-//   scene.add(shape);
-// }
-
-// Array(200).fill().forEach(addShape);
+  scene.add(knot);
+}
+Array(50).fill().forEach(randomTorusKnot);
 
 const cubeTexture = new THREE.TextureLoader().load(
   `${process.env.PUBLIC_URL}/imgs/cubes.png`
@@ -64,17 +78,30 @@ scene.background = cubeTexture;
 export function animate() {
   requestAnimationFrame(animate);
 
-  //   torusKnot.rotation.x += 0.002;
-  //   torusKnot.rotation.y += 0.002;
+  torusKnot.rotation.x += 0.002;
+  torusKnot.rotation.y += 0.002;
 
-  //   controls.update();
+  controls.update();
 
   renderer.render(scene, camera);
 }
 
-// function moveCamera() {
-//   const t = document.body.getBoundingClientRect().top;
-//   camera.position. = t * -0.5;
-// }
+window.scroll(0, 0);
+let lastScrollTop = 0;
+function moveCamera() {
+  const t = document.body.getBoundingClientRect().top;
+  camera.position.z = t * -0.005;
 
-// document.body.onscroll = moveCamera;
+  // or window.addEventListener("scroll"....
+  const st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+  if (st > lastScrollTop) {
+    // downscroll code
+    torusKnot.position.z -= 1;
+  } else {
+    // upscroll code
+    torusKnot.position.z += 1;
+  }
+  lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}
+
+document.body.onscroll = moveCamera;
